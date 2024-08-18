cfea7707d58d33e02e342cd4badb6866fd533bbd
shrivatsashetty <shrivatsa307@gmail.com>
2024-08-18 20:12:14 +0530

    added a form to take transaction as input

---
+++ sections/add_expense.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("# Add Expenses :floppy_disk:")
+st.markdown("***")
+
+with st.form(key = "_add_exp_form"):
+    st.markdown("### Add Expense :moneybag:")
+    amount = st.number_input(label = "Enter Amount",min_value = 1, placeholder = "Enter Transaction Amount")
+    note = st.text_input(label = "Add Note", )
+    expense_category = st.selectbox(label = "Expense Category", options = ["Rent 💵", "Tax 💶", "Food 🥞", "Travel 🚌", "Investment 🏦"])
+
+
+    if st.form_submit_button("Add", type = 'primary' ):
+        st.success('Expense Added Sucessfully', icon="✅")
+
