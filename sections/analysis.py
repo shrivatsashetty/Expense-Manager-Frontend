63f7981d402f7ccdb36766189a154bf40221d9a7
shrivatsashetty <shrivatsa307@gmail.com>
2024-08-24 12:29:33 +0530

    removed unwanted lines of code

--- sections/analysis.py
+++ sections/analysis.py
@@ -15,74 +15,78 @@ try:
 
     if get_response.status_code == 200:
 
+        # transaction data sent as response by the API
         transactions_data = get_response.json()
-        st.markdown("### Transactions Data:")
-        st.write(get_response.json())
 
         transactions_df = pd.DataFrame(transactions_data)
-        st.dataframe(transactions_df)
+        
+        if len(transactions_df) > 0:
+            st.markdown("***") # this adds a horizontal ruler
+            st.markdown("## Key-Metrics :key:")
 
-        st.markdown("***") # this adds a horizontal ruler
-        st.markdown("## Key-Metrics :key:")
+            st.markdown("### Transactions Data:")
+            st.dataframe(transactions_df)
 
-        col11, col12, col13 = st.columns(3, gap ='small', vertical_alignment = 'bottom')
+            col11, col12, col13 = st.columns(3, gap ='small', vertical_alignment = 'bottom')
 
-        with col11:
-            st.metric(
-                label = ":blue[Total Transactions]" , 
-                value = len(transactions_df['transactionAmount']) ,
-                help = "Username" , 
-            )
+            with col11:
+                st.metric(
+                    label = ":blue[Total Transactions]" , 
+                    value = len(transactions_df['transactionAmount']) ,
+                    help = "Username" , 
+                )
 
-        with col12:
-            st.metric(
-                label = ":green[Avg Spending Per Day]",
-                value = int(transactions_df['transactionAmount'].mean()),
-                delta = "5.4%", # indicates rate of increase or deacrease
-                delta_color = "normal", # indicates color coding for delta value
-                help = "Total Income for the Current Month"
-            )
+            with col12:
+                st.metric(
+                    label = ":green[Avg Spending Per Day]",
+                    value = int(transactions_df['transactionAmount'].mean()),
+                    delta = "5.4%", # indicates rate of increase or deacrease
+                    delta_color = "normal", # indicates color coding for delta value
+                    help = "Total Income for the Current Month"
+                )
 
-        with col13:
-            st.metric(
-                label = ":red[Max Single Transaction]",
-                value = transactions_df['transactionAmount'].max(),
-                delta = "6.2%",
-                delta_color = "inverse",
-                help = "Total Expenditure so far for the Current Month"
-            )
+            with col13:
+                st.metric(
+                    label = ":red[Max Single Transaction]",
+                    value = transactions_df['transactionAmount'].max(),
+                    delta = "6.2%",
+                    delta_color = "inverse",
+                    help = "Total Expenditure so far for the Current Month"
+                )
 
-        st.markdown("***")
-        st.markdown("## Category Wise Expenditure")
+            st.markdown("***")
+            st.markdown("## Category Wise Expenditure")
 
-        # Displaying the DataFrame
-        fig = px.pie(transactions_df, values = 'transactionAmount', names = 'expenseCategory', hole=.2, )
-        st.plotly_chart(fig, use_container_width=True)
+            # Displaying the DataFrame
+            fig = px.pie(transactions_df, values = 'transactionAmount', names = 'expenseCategory', hole=.2, )
+            st.plotly_chart(fig, use_container_width=True)
 
-        st.markdown("***")
-        st.markdown("## Month-Wise Expenditure")
+            st.markdown("***")
+            st.markdown("## Month-Wise Expenditure")
 
-        st.dataframe(transactions_df)
+            st.dataframe(transactions_df)
 
-        fig = px.bar(
-            transactions_df, 
-            x = 'transactionMonth', 
-            y = 'transactionAmount', 
-            title = 'Total Monthly Expenditure So Far',
-            color = 'expenseCategory',
-            )
+            fig = px.bar(
+                transactions_df, 
+                x = 'transactionMonth', 
+                y = 'transactionAmount', 
+                title = 'Total Monthly Expenditure So Far',
+                color = 'expenseCategory',
+                )
 
-        st.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True)
 
-        # crating data to monitor daily spending trends
-        st.markdown("***")
-        st.markdown("## Daily Spending Trends")
- 
-        fig = px.line(transactions_df, x='transactionDate', y='transactionAmount', color='transactionMonth', 
-                    title='Daily Spending Trends for Past and Current Month')
+            # crating data to monitor daily spending trends
+            st.markdown("***")
+            st.markdown("## Daily Spending Trends")
+    
+            fig = px.line(transactions_df, x='transactionDate', y='transactionAmount', color='transactionMonth', 
+                        title='Daily Spending Trends for Past and Current Month')
 
-        st.plotly_chart(fig, use_container_width=True)
-
+            st.plotly_chart(fig, use_container_width=True)
+        
+        else:
+            st.markdown("### Empty Data by API")
 except Exception as err:
-    st.markdown("### Data Not Found")
+    st.markdown("### Server Not Found")
     st.write(err)
