73f1cf7af380f9bc5fb6ec6a5f5be0f8750e50b3
shrivatsashetty <shrivatsa307@gmail.com>
2024-08-25 22:34:08 +0530

    the send report is now functional

--- sections/send_report.py
+++ sections/send_report.py
@@ -1,15 +1,52 @@
+from io import StringIO
 import streamlit as st
 
-st.markdown("# Send Report :envelope:")
+# modules for validating email
+from validate_email import validate_email_or_fail
+from validate_email.exceptions import EmailValidationError
+
+from custom_packages.email_operations import send_email
+
+st.markdown("# Mail Report :envelope:")
 st.markdown("***")
 
 with st.form(key = "_add_exp_form"):
-    st.markdown("### Send Report :speech_balloon:")
-    email = st.text_input(label = "Email Address", placeholder = "Enter E-Mail Address of the Recepient")
-    subject = st.text_input(label = "Enter Subject", placeholder = "Enter Subject for the E-Mail")
-    st.file_uploader(label = "Upload the Report File", type = ["pdf", "png", "jpg"])
+    st.markdown("### Send Report Form :speech_balloon:")
+    recepient_email = st.text_input(label = "Email Address", placeholder = "Enter E-Mail Address of the Recepient")
+    email_subject = st.text_input(label = "Enter Subject", placeholder = "Enter Subject for the E-Mail")
+    email_body = st.text_area(label = "Email Body(Max 500 Characters)", placeholder = "Hello Sir/Ma'am...", max_chars = 500) 
+    email_attachment = st.file_uploader(label = "Upload the Report File", type = ["pdf", "png", "jpg"] )
+
+    sender_email = st.secrets.outlook_credentials.outlook_mail_id
+    sender_password = st.secrets.outlook_credentials.outlook_password
+
+    if email_attachment is not None:
+        attachment_bytes = email_attachment.getvalue()
+        attachment_file_name = email_attachment.name
 
 
     if st.form_submit_button("Send", type = 'primary' ):
-        st.error('Unable to Send, Invalid Email', icon="⛔")
-        st.toast("Fialed to Send, Try Again", icon="⚠️")
+        # st.write(type(email_attachment))
+        # st.write(email_attachment)
+
+        try:
+            # checks for validity of email adress first
+            validate_email_or_fail(email_address = recepient_email, dns_timeout = 20)
+
+            # the mail is sent only if the email validation passes
+            send_email(
+                sender_email,
+                sender_password,
+                recepient_email,
+                email_subject,
+                email_body,
+                # email_attachment,
+                attachment_bytes,
+                attachment_file_name
+            )
+        except EmailValidationError as email_err:
+            st.warning("Invalid Email Address", icon = "🚫")
+            # st.write(email_err)
+        except Exception as exception:
+            st.warning("Unable to Send Email, An Error Occoured", icon = "🚫")
+            # st.write(exception)
